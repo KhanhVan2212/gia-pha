7d0b4b56472a8b9c81a8725672bee41e1f9013f3
Khanhvan2212 <nguyenkhanhvan221202@gmail.com>
2026-03-02 10:53:21 +0700

    Update: Phân quyền

--- frontend/src/app/(main)/layout.tsx
+++ frontend/src/app/(main)/layout.tsx
@@ -1,5 +1,6 @@
 import { Sidebar } from "@/components/layout/sidebar";
 import { Header } from "@/components/layout/header";
+import { ProtectedRoute } from "@/components/protected-route";
 
 export default function MainLayout({
   children,
@@ -7,14 +8,16 @@ export default function MainLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="flex min-h-screen">
-      <Sidebar />
-      <div className="flex flex-1 min-w-0 flex-col">
-        <Header />
-        <main className="flex-1 min-w-0 overflow-hidden p-4 lg:p-6">
-          {children}
-        </main>
+    <ProtectedRoute>
+      <div className="flex min-h-screen">
+        <Sidebar />
+        <div className="flex flex-1 min-w-0 flex-col">
+          <Header />
+          <main className="flex-1 min-w-0 overflow-hidden p-4 lg:p-6">
+            {children}
+          </main>
+        </div>
       </div>
-    </div>
+    </ProtectedRoute>
   );
 }
